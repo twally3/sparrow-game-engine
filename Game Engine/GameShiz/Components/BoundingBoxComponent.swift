995c580c58d26866f45f67566672ef54ed1ba538
Max Taylor <max@cakerstream.com>
2020-05-17 19:18:36 +0100

    Checkpoint: Initial single axis implementation

---
+++ Game Engine/GameShiz/Components/BoundingBoxComponent.swift
@@ -0,0 +1,9 @@
+//
+//  BoundingBoxComponent.swift
+//  Game Engine macOS
+//
+//  Created by Max Taylor on 16/05/2020.
+//  Copyright © 2020 Max Taylor. All rights reserved.
+//
+
+import Foundation
---
+++ Game Engine/GameShiz/Systems/CollisionSystem.swift
@@ -0,0 +1,9 @@
+//
+//  CollisionSystem.swift
+//  Game Engine macOS
+//
+//  Created by Max Taylor on 16/05/2020.
+//  Copyright © 2020 Max Taylor. All rights reserved.
+//
+
+import Foundation
